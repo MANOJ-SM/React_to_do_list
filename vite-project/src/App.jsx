7cd958c161ff761da40c1c5dbed0feaadeb481bc
MANOJ_SM <manumedleri@gmail.com>
2025-02-13 08:10:52 +0530

    added components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,16 @@
+import React, { useState } from 'react';
+import Header from './Header';
+import ToDoList from './ToDoList';
 
 function App() {
-  return (
-    <>
+  const [todos, setTodos] = useState([]); // State to hold the to-do items
 
-    </>
-  )
-  
+  return (
+    <div className="app">  {/* Basic styling - you'll improve this */}
+      <Header />
+      <ToDoList todos={todos} /> {/* Pass todos as props */}
+    </div>
+  );
 }
 
-export default App
+export default App;
